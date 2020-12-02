ecc23e0979a13184145c4adc3a1120affec67298
Mats Andreassen <matsand@live.no>
2020-12-02 11:12:25 +0100

    La til test av function

---
+++ tests/generate-document.test.js
@@ -0,0 +1,51 @@
+const generateDocumentFunc = require('../GenerateDocument')
+const { writeFile } = require('fs').promises
+const { join } = require('path')
+
+const varselFagContent = require('./data/varsel-fag.json')
+
+const context = { log: console.log, invocationId: 'testing' }
+
+describe('GenerateDocument function test', () => {
+  it('returns 400 when no body was provided', async () => {
+    await expect(generateDocumentFunc(context, {})).resolves
+      .toMatchObject({
+        status: 400,
+        body: { error: { message: 'No request body received!' } }
+      })
+  })
+
+  it('returns 400 if schema validation fails', async () => {
+    await expect(generateDocumentFunc(context, { body: {} })).resolves
+      .toMatchObject({
+        status: 400,
+        body: { error: { message: 'Invalid request body' } }
+      })
+  })
+
+  it('returns 404 if template wasn\'t found', async () => {
+    await expect(generateDocumentFunc(context, { body: { system: 'minelev', template: 'not-found' } })).resolves
+      .toMatchObject({
+        status: 404,
+        body: { error: { message: 'Template not found!' } }
+      })
+  })
+
+  it('returns 200 if template was found', async () => {
+    await expect(generateDocumentFunc(context, { body: varselFagContent })).resolves
+      .toMatchObject({
+        status: 200
+      })
+  })
+
+  it('can parse and write returned base64 to .pdf file correctly', async () => {
+    const document = await generateDocumentFunc(context, { body: varselFagContent })
+    const { base64 } = document.body.data
+    await expect(typeof base64).toBe('string')
+
+    const buffer = Buffer.from(base64, 'base64')
+    await expect(buffer.byteLength).toBeGreaterThan(50000)
+
+    await writeFile(join(__dirname, '/data/varsel-fag.pdf'), buffer)
+  })
+})
