14c17a5c3f33d3e6684e64db9a6f32d13b04382e
Mats Andreassen <matsand@live.no>
2020-12-02 11:11:54 +0100

    La til test for sjekk av maler/definitions

---
+++ tests/lib/get-template.test.js
@@ -0,0 +1,23 @@
+const getTemplate = require('../../lib/get-template')
+
+describe('Get template', () => {
+  it('throws when system isn\'t found', async () => {
+    await expect(getTemplate({ system: 'not-found', template: 'not-found' })).rejects
+      .toThrow('Template not found!')
+  })
+
+  it('throws when template isn\'t found', async () => {
+    await expect(getTemplate({ system: 'minelev', template: 'not-found' })).rejects
+      .toThrow('Template not found!')
+  })
+
+  it('fallbacks to \'nb\' when template for language isn\'t found', async () => {
+    await expect(getTemplate({ system: 'minelev', template: 'varsel-fag', language: 'en' })).resolves
+      .toContain('language: nb')
+  })
+
+  it('returns template', async () => {
+    await expect(getTemplate({ system: 'minelev', template: 'varsel-fag' })).resolves
+      .toContain('---\ndefinition: brevmal\nlanguage: nb')
+  })
+})
